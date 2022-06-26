5164e961e24a19bf38522bc60e4b06359bd2a519
BiswasAmitav <biswasamit2648@gmail.com>
2022-06-26 22:13:57 +0530

    on this code i practiced some interested multiplication table

---
+++ multiplicationTable.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int selectPair,number1,startNumber,endNumber;
+
+    cout << "For single pair , Press 1 & For multipair, Press 2 : " <<endl;
+    cin >> selectPair;
+
+    if(selectPair==1){
+
+        cout << "Entered your number : " <<endl;
+        cin >> number1;
+        
+            for(int i=number1;i<=number1;i++){
+                for(int j=1;j<=10;j++){
+                    cout << i << " * " << j << " = " << i*j << endl;
+                }
+            cout << endl;
+            }
+    }
+    else if(selectPair==2){
+        cout << "Start Number : " << " , ";
+        cout << "End Number : " << endl;
+
+        cin>>startNumber >>endNumber;
+
+        for(int i=startNumber;i<=endNumber;i++){
+                for(int j=1;j<=10;j++){
+                    cout << i << " * " << j << " = " << i*j << endl;
+                }
+            cout << endl;
+            }
+
+    }
+    else
+        cout<<"You entered wrong number ...!";
+
+    return 0;
+}
