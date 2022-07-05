0ffd77f2031a49b0ba6e2a3c4bdf47644bd644b7
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-05 14:57:44 +0530

    some challenges

--- DSA/sortingAlgoPractice/latestBubbl.cpp
+++ DSA/sortingAlgoPractice/latestBubbl.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 void BubbleSort(int array[], int n){
-                    int temp;
+        int temp;
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-1;j++){
-
                 temp=array[j];
                 array[j]=array[j+1];
                 array[j+1]=temp;
             }
         }
-            cout<<"Result : after sorted :-"<<"\n";
-    for(int i=0;i<n;i++){
-        cout<<array[i]<<"\t";
-    }
+        cout<<"Result : after sorted :-"<<"\n";
+        for(int i=0;i<n;i++){
+            cout<<array[i]<<"\t";
+        }
 }
 int main(){
     int n;
